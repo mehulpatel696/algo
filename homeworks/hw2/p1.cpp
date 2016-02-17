0f0d8918fd6547c7202b398218bb3e282b5b4e27
Mehul Patel <mehulpatel696@gmail.com>
2016-02-17 10:09:39 -0500

    added template

--- homeworks/hw2/p1.cpp
+++ homeworks/hw2/p1.cpp
@@ -1,52 +1,57 @@
-
-#include<iostream>
-#include<cstdlib>
-#include<vector>
-#include<sstream>
-#include<algorithm>
-#include<unordered_set>
+#include <cstdio>
+#include <string.h>
+#include <climits>
+#include <math.h>
+#include <iostream>
 using namespace std;
 
+#define min(a,b) if (a > b) a = b
+#define set(data, n) for(int i = 0;  i < n; i++) data[i] = 0;
 
-#define rem(l,rm) for(vector<string>::iterator it = l.begin(); it != l.end(); it++) (*it).erase((*it).begin() + rm);
-#define insert(set, vec) for(int i = 0; i < vec.size(); i++) set.insert(vec[i]);
-
-
-int min_leds_needed(int rm, vector<string> l, int d){
-    rem(l,rm);
-    int m_d  = 0;
-    unordered_set<string> ss;
-    insert(ss,l);
-    bool val = (ss.size() == l.size()) ? false : true;
-    if(val) return d;
-    else for(int i = 0; i < l[0].size(); i++) m_d = max(m_d,min_leds_needed(i, l,  d + 1));
-    return m_d;
-    return 0;
+int cb(int m){
+    return (m == 0) ? 0 : cb(m & (m-1)) + 1;
 }
 
-
-int main()
-{
+int main() {
+    int data[105];
+    /* (int) pow(2,15) = 1 << 15 */
+    bool on[(int) pow(2,15)];
     int testcases;
     cin>>testcases;
     while(testcases--){
-        int p,n,t;
+        int p,n;
         cin>>p>>n;
-        t = p;
-        vector<string> leds;
-        while(n--){
-            string led = "";
-            while(p--){
-                char l;
-                cin>>l;
-                led += l;
+        set(data,n);
+        for(int i = 0;  i < n; i++){
+           /*set ith data element to 0 */
+           for(int j = 0; j < p; j++){
+                int c;
+                cin>>c;
+                /* if the input bit is poitive then set the ith bit to the  0 | 2 ^ j*/
+               if(c) {
+                   cout<<"Before: "<<data[i];
+                   data[i] |= ( 1 << j);
+                   cout<<" After: "<<data[i]<<endl;
+               }
             }
-            p = t;
-            leds.push_back(led);
         }
-        int m = 0;
-        for(int i = 0; i < p; i++) m = max(m, min_leds_needed(i, leds, 0));
-        cout<<p - m<<endl;
+        int min_leds = INT_MAX;
+        for(int i = 0; i < (1 << p); i++){
+            size_t s= sizeof(on);
+            memset(on, false, s);
+            bool f = true;
+            for(int j = 0; j < n; j++){
+                if(on[i & data[j]]){
+                    f = false;
+                    break;
+                }
+                on[i & data[j]] = true;
+            }
+            if(f) min(min_leds, cb(i));
+        }
+
+
+        cout<<min_leds<<endl;
     }
-    return 0;
 }
+
---
+++ homeworks/hw2/template.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <bitset>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <set>
+#include <utility>
+#include <algorithm>
+#include <stack>
+#include <queue>
+using namespace std;
+
+
+#define fill_a (a,j) for(int i = 0; i < j; j++) a[i] = i;
+#define fill_v (a,j) for(int i = 0; i < j; j++) a.push_back(i);
+
+
+
+
+
+int main(){
+
+    int testcases;
+    cin>>testcases;
+    while(testcases--){
+
+    }
+
+}
